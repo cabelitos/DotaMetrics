d76d44705120405fc0c82156d5c2c4f722b29e85
cabelitos <cabelitos@uol.com.br>
2014-12-24 00:37:46 -0200

    Set the default log handler to file.
    As this will run in a server, we must log in a file to check if something
    wrong happned

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,50 @@
 #include <QCoreApplication>
+#include <QtGlobal>
+#include <cstdlib>
+#include <QByteArray>
+#include <QFile>
+#include <QDir>
+#include <QString>
+#include <QTextStream>
 #include "Crawler.hpp"
 
+static void _myMessageOutput(QtMsgType type,
+			     const QMessageLogContext &context,
+			     const QString &msg) {
+  Q_UNUSED(context);
+  QByteArray localMsg = msg.toLocal8Bit();
+  QFile output(QDir::currentPath() + QDir::separator() + "log.txt");
+  //Prey for the gods, this can't fail!
+  output.open(QIODevice::Text | QIODevice::Append);
+  QTextStream stream(&output);
+  switch (type) {
+  case QtDebugMsg:
+    stream << localMsg.constData() << "\n";
+    break;
+  case QtWarningMsg:
+    stream << "Warning:" << localMsg.constData() << "\n";
+    break;
+  case QtCriticalMsg:
+    stream << "Critical:" << localMsg.constData() << "\n";
+    break;
+  case QtFatalMsg:
+    stream << "Fatal:" << localMsg.constData() << "\n";
+    abort();
+    break;
+  default:
+    stream << "Default:" << localMsg.constData() << "\n";
+    break;
+  }
+  stream.flush();
+  output.close();
+}
+
 int main(int argc, char **argv) {
-    QCoreApplication app(argc, argv);
-    ApiRequester::getInstance("a");
-    Crawler c;
-    c.start();
-    app.exec();
-    return 0;
+  qInstallMessageHandler(_myMessageOutput);
+  QCoreApplication app(argc, argv);
+  ApiRequester::getInstance("a");
+  Crawler c;
+  c.start();
+  app.exec();
+  return 0;
 }
