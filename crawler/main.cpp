0c0fe5c5ebe35a083788e6c80cfa9843c40779f4
cabelitos <cabelitos@uol.com.br>
2015-06-27 13:01:33 -0300

    Add user option to save log in a file

--- crawler/main.cpp
+++ crawler/main.cpp
@@ -2,14 +2,17 @@
 #include <QtGlobal>
 #include <cstdlib>
 #include <QByteArray>
+#include <QStringList>
 #include <QFile>
 #include <QDir>
 #include <QString>
 #include <QTextStream>
 #include <QCommandLineParser>
-#include <iostream>
+#include <QCommandLineOption>
 #include <QDateTime>
 #include "Crawler.hpp"
+
+static QString _logFilePath = QString();
 
 static void _myMessageOutput(QtMsgType type,
 			     const QMessageLogContext &context,
@@ -17,9 +20,9 @@ static void _myMessageOutput(QtMsgType type,
   Q_UNUSED(context);
   QString date = QDateTime::currentDateTime().toString("h:m:s  d/M/yyyy t");
   QByteArray localMsg = msg.toLocal8Bit();
-  QFile output(QDir::currentPath() + QDir::separator() + "log.txt");
-  //Prey for the gods, this can't fail!
-  output.open(QIODevice::Text | QIODevice::Append);
+  QFile output(_logFilePath + QDir::separator() + "log.txt");
+  if (!output.open(QIODevice::Text | QIODevice::Append))
+    return;
   QTextStream stream(&output);
   bool mustAbort = false;
   switch (type) {
@@ -47,21 +50,30 @@ static void _myMessageOutput(QtMsgType type,
 }
 
 int main(int argc, char **argv) {
-  qInstallMessageHandler(_myMessageOutput);
   QCoreApplication app(argc, argv);
   QCommandLineParser parser;
   QCoreApplication::setApplicationName("Dota Metrics Crawler");
   QCoreApplication::setApplicationVersion("1.0");
   parser.setApplicationDescription("Fetches data from the dota restful api");
+  QCommandLineOption logFileOption(QStringList() << "l" << "log",
+				   "log file path",
+				   "log");
+  parser.addOption(logFileOption);
   parser.addHelpOption();
   parser.addVersionOption();
   parser.addPositionalArgument("key", "Your Steam API key");
   parser.process(app);
   const QStringList args = parser.positionalArguments();
   if (args.isEmpty()) {
-    std::cout << "You need to provide an API key\n";
+    qCritical() << "You need to provide an API key\n";
     parser.showHelp();
     return -1;
+  }
+
+  _logFilePath = parser.value(logFileOption);
+  if (!_logFilePath.isEmpty()) {
+    qDebug() << "Saving log at:" << _logFilePath << "\n";
+    qInstallMessageHandler(_myMessageOutput);
   }
   ApiRequester::getInstance(args.at(0));
   Crawler c;
